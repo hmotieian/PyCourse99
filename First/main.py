95090859d33a24b40d6fa13cc4caa5615b506ba2
hmotieian <hmotieian@gmail.com>
2020-11-30 16:47:25 +0330

    Test sns

--- First/main.py
+++ First/main.py
@@ -1,3 +1,18 @@
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 print("Hello World")
+x = 10
+
+sns.set_theme(style="ticks")
+
+# Load the example dataset for Anscombe's quartet
+df = sns.load_dataset("anscombe")
+
+# Show the results of a linear regression within each dataset
+sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df,
+           col_wrap=2, ci=None, palette="muted", height=4,
+           scatter_kws={"s": 50, "alpha": 1})
+plt.show()
